d9fb7a248bc416895f9b0e22e2cae031620b6590
vedangJavdekar <vedang.javdekar@gmail.com>
2019-12-10 19:03:56 +0530

    Navigation new codes Tested

---
+++ Navigation_Tested_Codes/Navigation_Tested_Codes.ino
@@ -0,0 +1,208 @@
+#include "flags.h"
+#include <AccelStepper.h>
+#include <Servo.h>
+#include <Wire.h>
+#include "MPU6050.h"
+
+/********** VARIABLES *******/
+const char* NavFormat = "%d, %d, %d, %d";
+int vel[4] = {0};
+String data, prevData;
+
+
+
+/**** PUMP SOLENOID *****/
+#define PUMP_PIN 50
+#define SOL_PIN 48
+
+void PumpSetup()
+{
+  pinMode(PUMP_PIN, OUTPUT);
+  pinMode(SOL_PIN, OUTPUT);
+  digitalWrite(PUMP_PIN, LOW);
+  digitalWrite(SOL_PIN, LOW);
+}
+
+void PumpVel(int z)
+{
+  if (z != 0)
+  {
+    if (z < 0)
+    {
+      digitalWrite(PUMP_PIN, LOW);
+      digitalWrite(SOL_PIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(PUMP_PIN, HIGH);
+      digitalWrite(SOL_PIN, HIGH);
+    }
+  }
+  else
+  {
+    digitalWrite(PUMP_PIN, LOW);
+    digitalWrite(SOL_PIN, LOW);
+  }
+}
+
+/*********** MPU AND STEPPER ****************/
+
+int pitch=0, roll, flag, prev_pitch=0;
+int speed = 0;
+MPU6050 mpu;
+AccelStepper stepper(1, STEPPER_PUL, STEPPER_DIR);
+
+void PitchCorrect()
+{
+  Vector normAccel = mpu.readNormalizeAccel();
+  delay(1);
+  pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
+  //roll = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
+  if (PITCH_TOLERANCE - abs(pitch) < 0)
+  {
+    if (pitch > (PITCH_OFFSET + PITCH_TOLERANCE))
+    {
+      speed = -STEPPER_SPEED;
+    }
+    else if (pitch < (PITCH_OFFSET - PITCH_TOLERANCE))
+    {
+      speed = STEPPER_SPEED;
+    }
+    else
+    {
+      speed = 0;
+    }
+    stepper.setSpeed(speed);
+    stepper.runSpeed();
+  }
+  prev_pitch = pitch;
+}
+
+/*******************    THRUSTERS   ***********************/
+Servo thrusters[6];
+int servoPin[] = {3, 4, 5, 6, 7, 8};
+/* Z_DIRECTION
+   +-----------------------+
+   | 0                   1 |
+   |                       |
+   |                       |
+   |                       |
+   |          2            |
+   +-----------------------+
+   Y_DIRECTION
+   +-----------------------+
+   |                       |
+   |                       |
+   | 3                   4 |
+   |                       |
+   |                       |
+   +-----------------------+
+   X_DIRECTION
+   +-----------------------+
+   |                       |
+   |                       |
+   |          5            |
+   |                       |
+   |                       |
+   +-----------------------+
+*/
+void MoveThrusters()
+{
+  if (vel[0] != 0)
+  {
+    if (vel[0] > 0)
+    {
+      thrusters[HORZ].writeMicroseconds(T_POS);
+    }
+    else
+    {
+      thrusters[HORZ].writeMicroseconds(T_NEG);
+    }
+  }
+  else
+  {
+    thrusters[HORZ].writeMicroseconds(T_ZERO);
+  }
+
+  if (vel[1] != 0)
+  {
+    if (vel[1] > 0)
+    {
+      thrusters[FWD_1].writeMicroseconds(T_POS);
+      thrusters[FWD_2].writeMicroseconds(T_POS);
+    }
+    else
+    {
+      thrusters[FWD_1].writeMicroseconds(T_NEG);
+      thrusters[FWD_2].writeMicroseconds(T_NEG);
+    }
+  }
+  else
+  {
+    thrusters[FWD_1].writeMicroseconds(T_ZERO);
+    thrusters[FWD_2].writeMicroseconds(T_ZERO);
+  }
+
+  if (vel[2] != 0)
+  {
+    if (vel[2] > 0)
+    {
+      thrusters[UP_1].writeMicroseconds(T_POS);
+      thrusters[UP_2].writeMicroseconds(T_POS);
+      thrusters[UP_3].writeMicroseconds(T_POS);
+
+    }
+    else
+    {
+      thrusters[UP_1].writeMicroseconds(T_NEG);
+      thrusters[UP_2].writeMicroseconds(T_NEG);
+      thrusters[UP_3].writeMicroseconds(T_NEG);
+    }
+  }
+  else
+  {
+    thrusters[UP_1].writeMicroseconds(T_ZERO);
+    thrusters[UP_2].writeMicroseconds(T_ZERO);
+    thrusters[UP_3].writeMicroseconds(T_ZERO);
+  }
+}
+
+/***************   MAIN CODE  *********************/
+void setup() {
+  Serial1.begin(9600);
+  Serial1.setTimeout(20);
+  
+  debug_pump(PumpSetup();)
+  debug_step(
+    stepper.setEnablePin(STEPPER_EN);
+    stepper.setMaxSpeed(STEPPER_MAXSPEED);
+    while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G));
+  )
+  debug_thr(
+  for (int i = 0; i < 6; i++)
+  {
+    thrusters[i].attach(servoPin[i]);
+    thrusters[i].writeMicroseconds(T_ZERO);
+    delay(100);
+  }
+  delay(7000);
+  )
+}
+
+void loop() {
+  
+  if (Serial1.available()) {
+    data = Serial1.readString();
+    Serial1.flush();
+    sscanf(&data[0], NavFormat, &vel[0], &vel[1], &vel[2],&vel[3]);
+  }
+  if (prevData != data)
+  {
+    debug_thr(MoveThrusters();)
+    debug_pump(PumpVel(vel[3]);)
+    Serial1.println(data);
+    Serial1.flush();
+  }
+  debug_step(PitchCorrect();)
+  prevData = data;
+}
---
+++ Navigation_Tested_Codes/flags.h
@@ -0,0 +1,24 @@
+/************** DEBUG FLAGS ****************/
+#define THR_ON 1
+#define PUMP_ON 1 //THIS IS  temp
+#define STEPPER_ON 1
+
+#define debug_thr(x) if(THR_ON){x}
+#define debug_pump(x) if(PUMP_ON){x}
+#define debug_step(x) if(STEPPER_ON){x}
+
+/*************** CONSTANTS **************/
+enum THRUST {UP_1, UP_2, UP_3, FWD_1, FWD_2, HORZ} thr;
+
+#define T_POS 1700
+#define T_NEG 1300
+#define T_ZERO 1500
+
+#define PITCH_TOLERANCE 10
+#define PITCH_OFFSET 0
+
+#define STEPPER_EN A1
+#define STEPPER_PUL 9
+#define STEPPER_DIR A0
+#define STEPPER_MAXSPEED 1600
+#define STEPPER_SPEED 1500
